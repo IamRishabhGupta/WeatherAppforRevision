8ffc72b160d11a6ee8642d50a408eee50d73640f
Rishabh Gupta <rishabh9aug@gmail.com>
2023-10-17 11:24:25 +0530

    Before Till Lottie animation on main activity_main.xml

--- app/src/main/java/com/example/weatherapp/SplashActivity.kt
+++ app/src/main/java/com/example/weatherapp/SplashActivity.kt
@@ -1,11 +1,21 @@
 package com.example.weatherapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                val intent=Intent(this,MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            },3000
+        )
         setContentView(R.layout.activity_splash)
     }
 }
